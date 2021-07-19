d6240ab96e41a4bdf26808965063f42b674367b9
Norman Robinson <normanrobinson@normans-mbp.attlocal.net>
2021-07-18 22:48:44 -0400

    logic.js

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,6 +1,6 @@
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
-var earthquakes= new L.LayerGroup();
+var earthquakes = new L.LayerGroup();
 
     var streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
         attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
@@ -15,3 +15,15 @@ var earthquakes= new L.LayerGroup();
         id: "mapbox.dark",
         accessToken: API_KEY
     });
+
+    var earthquakes = new L.LayerGroup();
+
+    var baseMaps = {
+        "Street Maps": streetmap, 
+        "Dark Map": darkmap
+    };
+
+    var overlayMaps = {
+        Earthquakes: earthquakes
+    };
+    
