eba7be0129aaae63cb52c177f5e7747c31cc6683
Norman Robinson <normanrobinson@normans-mbp.attlocal.net>
2021-07-18 22:32:24 -0400

    Leaflet-Step-1

--- Leaflet-Step-1/static/js/config.js
+++ Leaflet-Step-1/static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "YOUR API KEY HERE!";
+const API_KEY = "pk.eyJ1IjoibnJvYmluODMiLCJhIjoiY2txM3RzZXkwMTI5ZTJ3azEzNXIxbmkweiJ9.oIW6O7tu4JViWydJ7SO7UQ";
---
+++ Leaflet-Step-1/static/js/logic.js
@@ -0,0 +1,10 @@
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+
+var earthquakes= new L.LayerGroup();
+
+    var streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 18,
+        id: "mapbox.streets",
+        accessToken: API_KEY
+    });
